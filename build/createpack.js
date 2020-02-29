ee3a15376be102206edec171869a25c97f82d427
sridhar <dreamsri91@gmail.com>
2020-03-01 02:33:48 +0530

    build automation script added

---
+++ build/createpack.js
@@ -0,0 +1,44 @@
+const glob = require("glob");
+const config = require("./config.json");
+const fs = require('fs');
+const path = require('path');
+const pkgJSON = require('../package.json');
+const exec = require('child_process').exec;
+
+config.copyFilesForPack.forEach((e, i) => {
+    glob(e, {}, function (er, files) {
+        files.forEach((v, idx) => {
+            if (fs.existsSync(v) && fs.lstatSync(v).isDirectory() !== true) {
+                const des = v.split('/').slice(1).join('/');
+                const curPath = path.resolve('output/' + des);
+                console.log('Copying File from ' + v + ' to > ' + des)
+                ensureDirectoryExistence(curPath);
+                fs.copyFileSync(v, 'output/' + des);
+            }
+        });
+    });
+});
+
+const version = pkgJSON.version;
+// exec('cd ./output')
+exec('npm pack ./output');
+const fileName = `${pkgJSON.name}-${pkgJSON.version}.tgz`;
+const pkgPath = path.resolve('./' + fileName);
+if (!fs.existsSync('package')) {
+    fs.mkdirSync('package');
+}
+fs.copyFileSync(pkgPath, './package/' + fileName + '.tgz');
+
+console.log('------------------------------------------------------------')
+console.log('Your package is created!' + fileName + '.tgz')
+
+// exec('cd ..')
+
+function ensureDirectoryExistence(filePath) {
+    var dirname = path.dirname(filePath);
+    if (fs.existsSync(dirname)) {
+        return true;
+    }
+    ensureDirectoryExistence(dirname);
+    fs.mkdirSync(dirname);
+}
