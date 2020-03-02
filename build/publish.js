c6f3fb8f514268db0d8e91e4b7cb0411855a975f
Sridhar S <dreamsri91@gmail.com>
2020-03-02 14:00:55 +0530

    updated changes

--- build/publish.js
+++ build/publish.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 var myArgs = process.argv.slice(2);
 
 console.log(myArgs[0]);
+console.log(JSON.stringify(myArgs));
 
 fs.writeFileSync('.npmrc','//npm.pkg.github.com/@sridharsathasivam/:_authToken='+myArgs[0]);
 
