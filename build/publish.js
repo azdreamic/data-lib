8a9abca0a3413ccfc59293f5e14733b4b7c81fd9
Sridhar S <dreamsri91@gmail.com>
2020-03-02 13:57:43 +0530

    testing commit

--- build/publish.js
+++ build/publish.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 
+
 var myArgs = process.argv.slice(2);
+
+console.log(myArgs[0]);
 
 fs.writeFileSync('.npmrc','//npm.pkg.github.com/@sridharsathasivam/:_authToken='+myArgs[0]);
 
