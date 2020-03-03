98837d737c92184d57e857241d232b0596256f18
sridhar <dreamsri91@gmail.com>
2020-03-03 23:47:21 +0530

    npm publish auth steps on release

--- build/publish.js
+++ build/publish.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 
 var myArgs = process.argv.slice(2);
 
-fs.writeFileSync('.npmrc','//npm.pkg.github.com/@sridharsathasivam/:_authToken='+myArgs[0]);
+fs.writeFileSync('.npmrc', '//registry.npmjs.org/data-lib/:_authToken=' + myArgs[0]);
 
