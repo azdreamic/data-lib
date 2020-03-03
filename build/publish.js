0f9cf1b6d57cc1b84293949d953de06312f6ad24
sridhar <dreamsri91@gmail.com>
2020-03-04 00:07:23 +0530

    package npm publish test

--- build/publish.js
+++ build/publish.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 
 var myArgs = process.argv.slice(2);
 
-fs.writeFileSync('.npmrc', '//registry.npmjs.org/data-lib/:_authToken=' + myArgs[0]);
+fs.writeFileSync('.npmrc', '//registry.npmjs.org/:_authToken=' + myArgs[0]);
 
